bb676488601fb6e797d25eadb3b7c60fc73ada8d
semwei <semwei@omegapoint.se>
2023-11-08 12:48:33 +0100

    Added project for Garbage collection

---
+++ DotNet8.GarbageCollection/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+AppContext.SetData("GCHeapHardLimit", (ulong)1024 * 1024 * 1024); // 1024 MB
+Console.WriteLine("Current memory limit: {0} MB", ToMb(GC.GetGCMemoryInfo().TotalAvailableMemoryBytes));
+GC.RefreshMemoryLimit();
+Console.WriteLine("Current memory limit: {0} MB", ToMb(GC.GetGCMemoryInfo().TotalAvailableMemoryBytes));
+
+AppContext.SetData("GCHeapHardLimit", (ulong)50 * 1024 * 1024); // 50 MB
+GC.RefreshMemoryLimit();
+Console.WriteLine("New memory limit: {0} MB", ToMb(GC.GetGCMemoryInfo().TotalAvailableMemoryBytes));
+
+
+static double ToMb(double value) =>
+    (value / Math.Pow(1024, (int)2));
