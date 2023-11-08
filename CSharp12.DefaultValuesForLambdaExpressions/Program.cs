7c21bf9f296ff1c892963e65f03ba51df21d7fe3
semwei <semwei@omegapoint.se>
2023-11-08 12:48:11 +0100

    more example default values for lambda

--- CSharp12.DefaultValuesForLambdaExpressions/Program.cs
+++ CSharp12.DefaultValuesForLambdaExpressions/Program.cs
@@ -3,6 +3,8 @@
 
 var addWithDefault = (int addTo = 2) => addTo + 1;
 
+var IncrementBy = (int source, int increment = 1) => source + increment;
+
 var sum = (params int[] numbers) => numbers.Sum();
 
 
@@ -10,6 +12,11 @@ _ = addWithDefault(); // 3
 
 _ = addWithDefault(5); // 6
 
+_ = IncrementBy(5); // 6
+
+_ = IncrementBy(5, 2); // 7
+
 _ = sum(); // 0
 
 _ = sum(1, 2, 3);  // 6
+
