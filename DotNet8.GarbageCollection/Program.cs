6bf6276e0901415c91978dfab71ce19b992c49a8
semwei <semwei@omegapoint.se>
2023-11-08 12:52:17 +0100

    nicer

--- DotNet8.GarbageCollection/Program.cs
+++ DotNet8.GarbageCollection/Program.cs
@@ -1,14 +1,13 @@
 ﻿
 
 AppContext.SetData("GCHeapHardLimit", (ulong)1024 * 1024 * 1024); // 1024 MB
-Console.WriteLine("Current memory limit: {0} MB", ToMb(GC.GetGCMemoryInfo().TotalAvailableMemoryBytes));
+Console.WriteLine("Current memory limit: {0} MB", CurrentMemoryLimitInMb());
 GC.RefreshMemoryLimit();
-Console.WriteLine("Current memory limit: {0} MB", ToMb(GC.GetGCMemoryInfo().TotalAvailableMemoryBytes));
+Console.WriteLine("Current memory limit: {0} MB", CurrentMemoryLimitInMb());
 
 AppContext.SetData("GCHeapHardLimit", (ulong)50 * 1024 * 1024); // 50 MB
 GC.RefreshMemoryLimit();
-Console.WriteLine("New memory limit: {0} MB", ToMb(GC.GetGCMemoryInfo().TotalAvailableMemoryBytes));
+Console.WriteLine("New memory limit: {0} MB", CurrentMemoryLimitInMb());
 
 
-static double ToMb(double value) =>
-    (value / Math.Pow(1024, (int)2));
+static double CurrentMemoryLimitInMb() =>    GC.GetGCMemoryInfo().TotalAvailableMemoryBytes / Math.Pow(1024, (int)2);
